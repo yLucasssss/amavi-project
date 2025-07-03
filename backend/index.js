91d8ec1979d3217206ab22123cddbd9cc12b4b48
lucasaraujodasilva110@gmail.com <lucasaraujodasilva110@gmail.com>
2025-07-02 22:08:43 -0300

    fix: Ajusta configuração SSL para conexão com o banco

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,14 @@ app.use(express.json());
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: { "rejectUnauthorized": false } // Temporariamente desabilitado para depuração
+  ssl: { "rejectUnauthorized": false }
 });
 
 // Teste de conexão do banco de dados
